2c516e07fd96df064991c60e21a062d44cdcfb40
Mehriban Balayeva <mehribanbalayeva@MacBook-Air-Mehriban.local>
2021-07-09 14:19:10 +0400

    first commit

---
+++ src/reducer.js
@@ -0,0 +1,62 @@
+const reducer = (state, action) => {
+  if (action.type === "CLEAR_CART") {
+    return { ...state, cart: [] };
+  }
+
+  if (action.type === "DELETE_ITEM") {
+    return {
+      ...state,
+      cart: state.cart.filter((item) => item.id !== action.payload),
+    };
+  }
+
+  if (action.type === "INCREASE") {
+    let tempCart = state.cart.map((item) => {
+      if (item.id === action.payload) {
+        return { ...item, amount: item.amount + 1 };
+      }
+      return { ...item };
+    });
+    return { ...state, cart: tempCart };
+  }
+
+  if (action.type === "DECREASE") {
+    let tempCart = state.cart
+      .map((item) => {
+        if (item.id === action.payload) {
+          return { ...item, amount: item.amount - 1 };
+        }
+        return { ...item };
+      })
+      .filter((item) => item.amount !== 0);
+
+    return { ...state, cart: tempCart };
+  }
+
+  if (action.type === "GET_TOTALS") {
+    let { total, amount } = state.cart.reduce(
+      (cartTotal, cartItem) => {
+        const { price, amount } = cartItem;
+        cartTotal.total += amount * price;
+        cartTotal.amount += amount;
+        return cartTotal;
+      },
+      { total: 0, amount: 0 }
+    );
+    total = parseFloat(total.toFixed(2));
+
+    return { ...state, total, amount };
+  }
+
+  if (action.type === "LOADING") {
+    return { ...state, loading: true };
+  }
+
+  if (action.type === "FETCH_DATA") {
+    return { ...state, loading: false, cart: action.payload };
+  }
+
+  return state;
+};
+
+export default reducer;
